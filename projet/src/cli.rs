da704d3d42cd27fe5ee59d1193089246b9e9d4ef
Leyandre Roques <leyandre13@gmail.com>
2022-11-30 00:17:45 +0100

    changes on CLI, preparation to implement search command

--- projet/src/cli.rs
+++ projet/src/cli.rs
@@ -1,16 +1,26 @@
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 
 /// Représente les arguments en paramètres de ligne de commande
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[command(group(ArgGroup::new("commands").required(true).args(["scan","search"])))]
 pub struct CliArguments {
-    /// Commande à exécuter
-    #[arg(short, long)]
-    command: String,
 
-    /// Chemin où trouver les fichiers à analyser
-    #[arg(short, long)]
-    path: std::path::PathBuf,
+    /// (Needs --path)   Scan and fetch metadata's file at the pointed path
+    #[arg(long, conflicts_with = "kwrds", requires = "path")]
+    scan: bool,
+
+    /// (Used with --scan) Chemin où trouver les fichiers à analyser
+    #[arg(long, value_name = "PATH")]
+    path: Option<std::path::PathBuf>,
+
+    /// (Needs --kwrds)   Search keywords in directory containing previous metadata scanned and return file's names associated
+    #[arg(long, conflicts_with = "path", requires = "kwrds")]
+    search : bool,
+
+    /// (Used with --search) Keyword(s) used to filtered metadata's files
+    #[arg(long, value_name = "KEYWORDS")]
+    kwrds : String,
 }
 
 impl CliArguments {
@@ -19,6 +29,18 @@ impl CliArguments {
     }
 
     pub fn path(&self) -> &std::path::Path {
-        self.path.as_path()
+        self.path.as_ref().unwrap().as_path()
+    }
+
+    pub fn kwords(&self) -> &String {
+        &self.kwrds
+    }
+
+    pub fn scan(&self) -> bool {
+        self.scan
+    }
+
+    pub fn search(&self) -> bool {
+        self.search
     }
 }
--- projet/src/main.rs
+++ projet/src/main.rs
@@ -1,20 +1,32 @@
 use projet::cli::CliArguments;
 use projet::scan::scan;
+use std::path::Path;
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
+
+fn launch_scan(path : &Path) {
+    let music_files = scan(path);
+    for music_file in music_files {
+        println!("{:?}", music_file);
+    }
+}
+
+fn launch_search(_kw : &String) {
+    ()
 }
 
 fn main() {
     let args = CliArguments::new();
 
-    println!("{:?}", args.path());
+    if args.scan() {
+        println!("Scan succesfully started at : {:?}", args.path());
+        
+        launch_scan(args.path())
 
-    let music_files = scan(args.path());
-    for music_file in music_files {
-
-        let serialized = serde_json::to_string(&music_file).unwrap();
-        print_type_of(&serialized);
-        println!("{:?}", music_file);
+    } else if args.search() {
+        println!("Search succesfully started for : {:?}", args.kwords());
+        launch_search(args.kwords())
     }
 }
